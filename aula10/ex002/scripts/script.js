57562ec1be6938c7ec7c893f82b1d6aa70e989ab
Rodrigo Ferreira <rodrigoferreira981236@gmail.com>
2024-05-01 20:49:50 -0300

    update das datas e imagens

--- aula10/ex002/scripts/script.js
+++ aula10/ex002/scripts/script.js
@@ -1,20 +1,61 @@
-var nascimento  = document.getElementById("idata")
-var nasc = nascimento.value
-var detector = document.getElementById('detector')
-var ano = new Date()
-var anoAtual = ano.getFullYear()
-var form = document.getElementById('form')
-var suaidade = anoAtual - nascimento
-var masculino = document.getElementById('imasc')
-var feminino = document.getElementById('ifemi')
-var imagem1 = document.getElementById('img1')
-var imagem2 = document.getElementById('img2')
-var imagem3 = document.getElementById('img3')
-var imagem4 = document.getElementById('img4')
-var imagem5 = document.getElementById('img5')
-var imagem6 = document.getElementById('img6')
-var imagem7 = document.getElementById('img7')
-var imagem8 = document.getElementById('img8')
-var imagem9 = document.getElementById('img9')
-var imagem10 = document.getElementById('img10')
-var imagem11 = document.getElementById('img11')
+   
+    
+    var imagem2 = document.getElementById('img2')
+    var imagem3 = document.getElementById('img3')
+    var imagem5 = document.getElementById('img5')
+    var imagem6 = document.getElementById('img6')
+    var imagem8 = document.getElementById('img8')
+    var imagem10 = document.getElementById('img10')
+function clicou(){
+    var nascimento =document.getElementById('idata').value
+    var ano = new Date().getFullYear()
+    var idade = ano - Number(nascimento)
+    var img = document.getElementById('img')
+    var detector = document.getElementById('detector')
+    var imagem1 = document.getElementById('img1')
+
+    if(nascimento >= 2019){//nenem
+        img.style.display='block'
+        detector.innerText=`Voce tem ${idade} anos, é um nenem`
+        imagem1.style.display='block'
+        imagem3.style.display='none'
+        imagem6.style.display='none'
+    }else if(nascimento >= 2013 ){//criança
+        img.style.display='block'
+        detector.innerText=`voce tem ${idade} anos, é uma criança`
+        imagem1.style.display='none'
+        imagem5.style.display='none'
+        imagem3.style.display='block'
+    }else if(nascimento >= 2007){//adolescente
+        img.style.display='block'
+        detector.innerText=`voce tem ${idade} anos, é um adolescente`
+        imagem1.style.display='none'
+        imagem5.style.display='block' 
+        imagem6.style.display='none'
+        imagem3.style.display='none'
+    }else if(nascimento >= 1999){//jovem
+        img.style.display='block'
+        detector.innerText=`voce tem ${idade} anos, é um jovem`
+        imagem6.style.display='block'
+        imagem5.style.display='none'
+        imagem1.style.display='none'
+        imagem8.style.display='none'
+
+    }else if(nascimento >= 1975){//adulto
+        img.style.display='block'
+        detector.innerText=`voce tem ${idade} anos, é um adulto`
+        imagem8.style.display='block'
+        imagem5.style.display='none'
+        imagem6.style.display='none'
+        imagem1.style.display='none'
+        imagem10.style.display='none'
+    }else if(nascimento >=1974 || nascimento < 1974){
+        img.style.display='block'
+        detector.innerText=`voce tem ${idade} anos, é um senhor`
+        imagem10.style.display='block'
+        imagem8.style.display='none'
+        imagem5.style.display='none'
+        imagem6.style.display='none'
+        imagem1.style.display='none'
+    }
+}
