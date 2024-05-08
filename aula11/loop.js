fb7d6c019ff95779e0b1388b5d0d1f21807356e9
Rodrigo Ferreira <rodrigoferreira981236@gmail.com>
2024-05-07 21:17:25 -0300

    criação dos modelos

---
+++ aula11/loop.js
@@ -0,0 +1,18 @@
+var c = 1
+/*do{
+    console.log('-',c)
+    c++
+}while (c <= 10)
+*/
+
+/*while(c <= 10){
+    console.log('-',c)
+    c++
+}
+*/
+
+console.log('Vai começar!')
+for(var c; c<=5; c++){
+    console.log('--',c)
+}
+console.log('fim!!')
