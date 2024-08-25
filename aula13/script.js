c72297e3d81dba0ba93939e5ce5cb04a03d98db7
Rodrigo Ferreira <rodrigoferreira981236@gmail.com>
2024-08-24 21:41:39 -0300

    updt*2

--- aula13/script.js
+++ aula13/script.js
@@ -2,6 +2,7 @@ function tabuada(){
     let num = document.getElementById('numero')
     let contas = document.getElementById('contas')
     let n1 = Number(num.value)
+    let p = document.getElementById('p')
     contas.innerHTML=''
     if(num.value.length==0){
        window. alert('[ERRO] Digite um numero!')
@@ -10,12 +11,14 @@ function tabuada(){
             let linha = document.createElement('option')
             linha.innerHTML = `${n1} x ${c} = ${n1*c}`
             contas.appendChild(linha)
+            p.innerHTML='Pronto!'
         }
     }
 }
 function limpar(){
     let num = document.getElementById('numero')
     let contas = document.getElementById('contas')
+    p.innerHTML=''
     contas.innerHTML=''
     num.value=''
     let frase = document.createElement('option');
